02e7c949ae4b5d928f65e28d232940e8c68cc17f
eliashead <eliashead.costa@gmail.com>
2022-11-23 15:54:43 -0300

    feat: add stitches and _document with font-family roboto

---
+++ src/styles/index.ts
@@ -0,0 +1,38 @@
+import { createStitches } from '@stitches/react'
+
+export const {
+  config,
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+} = createStitches({
+  theme: {
+    colors: {
+      white: '#fff',
+
+      gray900: '#121214',
+      gray800: '#202024',
+      gray300: '#c4c4cc',
+      gray100: '#e1e1e6',
+
+      green500: '#00875f',
+      green300: '#00b37e',
+    },
+    fontSizes: {
+      md: '1.125rem',
+      lg: '1.25rem',
+      xl: '1.5rem',
+      '2xl': '2rem',
+    },
+  },
+  media: {
+    bp1: 'screen and (max-width: 768px)',
+    bp2: 'screen and (max-width: 640px)',
+    bp3: 'screen and (max-width: 540px)',
+    bp4: 'screen and (min-width: 1024px)',
+  },
+})
